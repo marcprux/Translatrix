a09b47d2b1409c7dff306fb43a37d96570074c51
Marc Prud'hommeaux <marc@skip.tools>
2025-01-29 11:17:31 -0500

    Add executable product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let package = Package(
     name: "Translatrix",
     platforms: [.macOS(.v13), .iOS(.v15)],
+    products: [
+        .executable(name: "translatrix", targets: ["Translatrix"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
     ],
